bd680c80b413c31e7e5fa64b309bd5ce673554b1
ByteMel <timmwangi02@gmail.com>
2023-11-14 06:23:59 +0300

    selects correct function

--- _printf.c
+++ _printf.c
@@ -1,51 +1,45 @@
 #include "main.h"
 
 /**
- * _printf - selects the correct function based on the format specifier
- * @format: identifier to look for
- * Return: the length of the resulting string
+ * _printf - selects correct function
+ * @format:identifier to look for
+ * Return:the length of string
  */
+
 int _printf(const char * const format, ...)
 {
-    convert conversions[] = {
-        {"%s", print_s},
-        {"%c", print_c},
-        {"%%", print_percent},
-        {"%i", print_i},
-        {"%d", print_d}
-    };
+	convert p[] = {
+		{"%s", print_s},
+		{"%c", print_c},
+		{"%%", print_37},
+		{"%i", print_i},
+		{"%d", print_d} };
 
-    va_list args;
-    int index = 0, array_size = 5, total_length = 0;
+	va_list args;
+	int in = 0, j, Tlen = 0;
 
-    va_start(args, format);
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return -1;
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-    while (format[index] != '\0')
-    {
-        int j = array_size - 1;
-        while (j >= 0)
-        {
-            if (conversions[j].placeholder[0] == format[index] &&
-                conversions[j].placeholder[1] == format[index + 1])
-            {
-                total_length += conversions[j].function(args);
-                index = index + 2;
-                goto ContinueLoop;
-            }
-            j--;
-        }
-
-        _putchar(format[index]);
-        total_length++;
-        index++;
-
-    ContinueLoop:
-        continue;
-    }
-
-    va_end(args);
-    return total_length;
+Here:
+	while (format[in] != '\0')
+	{
+		j = 4;
+		while (j >= 0)
+		{
+			if (p[j].ph[0] == format[in] && p[j].ph[1] == format[in + 1])
+			{
+				Tlen += p[j].function(args);
+				in = in + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[in]);
+		Tlen++;
+		in++;
+	}
+	va_end(args);
+	return (Tlen);
 }
-
