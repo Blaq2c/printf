ef964aac2098045c83097fc96786dbb3aecb4fab
ByteMel <timmwangi02@gmail.com>
2023-11-14 06:37:45 +0300

    prints string args

--- print_string.c
+++ print_string.c
@@ -1,33 +1,29 @@
 #include "main.h"
-
 /**
- * print_s - prints string arguments
- * @val: argument
- * Return: length of the string
+ * print_s - prints arguments
+ * @val:argument
+ * Return:length:the length of string
  */
+
 int print_s(va_list val)
 {
 	char *str;
 	int j, length;
 
 	str = va_arg(val, char *);
-
 	if (str == NULL)
 	{
 		str = "(null)";
 		length = _strlen(str);
-
-		for (j = 0; j < length; j++)
+		for (j = 0 ; j < length ; j++)
 			_putchar(str[j]);
 	}
 	else
 	{
 		length = _strlen(str);
-
 		for (j = 0; j < length; j++)
 			_putchar(str[j]);
 	}
-
 	return (length);
-}
 
+}
