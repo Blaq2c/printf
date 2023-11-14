15fc17ab9464fa281d66752dc1cbda33c05adcd6
ByteMel <timmwangi02@gmail.com>
2023-11-14 06:36:03 +0300

    prints an integer

--- print_int.c
+++ print_int.c
@@ -1,78 +1,92 @@
 #include "main.h"
-
 /**
- * print_i - prints integer
+ * print_i - prints intager
  * @args: argument to print
- * Return: number of characters printed
+ * Return: intager
  */
+
 int print_i(va_list args)
-
 {
-	int num = va_arg(args, int);
-	int digit, expn = 1, j = 0;
+	/*declare Variable;*/
+	int z = va_arg(args, int);
+	int number, last = z % 10, digit, expn = 1;
+	int j = 1;
 
-	if (num < 0)
+	/*extract the last digit*/
+	z = z / 10;
+	number = z;
+
+	/*check if the number is negative */
+	if (last < 0)
 	{
 		_putchar('-');
-		num = -num;
+		number = -number;
+		z = -z;
+		last = -last;
 		j++;
 	}
 
-	int temp = num;
-
-	while (temp / 10 != 0)
+	if (number > 0)
 	{
-		expn *= 10;
-		temp /= 10;
+		while (number / 10 != 0)
+		{
+			expn = expn * 10;
+			number = number / 10;
+		}
+		number = z;
+		while (expn >= 1)
+		{
+			digit = number / expn;
+			_putchar(digit + '0');
+			expn = expn / 10;
+			j++;
+		}
 	}
-
-	while (expn >= 1)
-	{
-		digit = num / expn;
-		_putchar(digit + '0');
-		num %= expn;
-		expn /= 10;
-		j++;
-	}
-
+	_putchar(last + '0');
 	return (j);
 }
 
 /**
  * print_d - prints decimal
  * @args: argument to print
- * Return: number of characters printed
+ * Return: intager
  */
+
 int print_d(va_list args)
-
 {
-	int num = va_arg(args, int);
-	int digit, expn = 1, j = 0;
+	int z = va_arg(args, int);
+	int number, last = z % 10, digit;
+	int j = 1;
+	int expn = 1;
 
-	if (num < 0)
+	z = z / 10;
+	number = z;
+
+	if (last < 0)
 	{
 		_putchar('-');
-		num = -num;
+		number = -number;
+		z = -z;
+		last = -last;
 		j++;
 	}
-
-	int temp = num;
-
-	while (temp / 10 != 0)
+	if (number > 0)
 	{
-		expn *= 10;
-		temp /= 10;
+		while (number / 10 != 0)
+		{
+			expn *= 10;
+			number /= 10;
+		}
+		number = z;
+		while (expn >= 1)
+		{
+			digit = z / expn;
+			_putchar(digit + '0');
+			z %= expn;
+			expn /= 10;
+			j++;
+		}
 	}
-
-	while (expn >= 1)
-	{
-		digit = num / expn;
-		_putchar(digit + '0');
-		num %= expn;
-		expn /= 10;
-		j++;
-	}
-
+	_putchar(last + '0');
 	return (j);
 }
-
